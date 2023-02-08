12f3fa301399c13b904bf568789ee92b2efc7f9d
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-02-08 22:20:36 +0900

    参考YoutubeURLを追加

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+#https://www.youtube.com/watch?v=bzbrpkbjWe8&t=981sで勉強
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!" 
+
 
 @app.route("/")
 def hello_world():
